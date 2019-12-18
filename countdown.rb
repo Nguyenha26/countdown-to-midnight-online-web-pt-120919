cc009a57239bfbf38394e9883f66daaf7d8d3606
Nguyen Ha <nynha0526@gmail.com>
2019-12-18 17:36:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -13,7 +13,7 @@ end
 def countdown_with_sleep(seconds_to_midnight)
   while seconds_to_midnight > 0
   sleep(1)
-  seconds_to_midnight -= log10
+  seconds_to_midnight -= 1
   end
   puts "HAPPY NEW YEAR" 
   
